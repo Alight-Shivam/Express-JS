2c268451e8f0e7701c7b4cad8faf2479213982e0
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-12-17 21:00:38 +0530

    Express JS

---
+++ Blogpoint/data/blogs.js
@@ -0,0 +1,20 @@
+{
+    blogs:[
+        {
+            title: "How to get started with python",
+            content: "This is content"
+        },
+        {
+            title: "How to get started with python",
+            content: "This is content"
+        },
+        {
+            title: "How to get started with python",
+            content: "This is content"
+        },
+        {
+            title: "How to get started with python",
+            content: "This is content"
+        }
+    ]
+}
